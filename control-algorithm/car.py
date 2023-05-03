5905dd697aeaa647ea99892ef6822701ff52c57b
Sonny <110838271+occult-engineer@users.noreply.github.com>
2023-05-03 11:30:58 +0100

    Comment and Code Corrections for Control Algorithm

--- control-algorithm/car.py
+++ control-algorithm/car.py
@@ -8,69 +8,83 @@ import radio
 
 # ========== Microbit Config
 radio.config(group=25, power=7, length=128, data_rate=radio.RATE_1MBIT)
-display.off()
+display.off() # Required to use ADC pins
 
 # ========== Pin Map
 steering_pin = pin0 # Potentiometer Smaller = Right, Larger = Left
 forw_p = pin6 # Drive Forward (Yellow)
 back_p = pin9 # Drive Backward (Blue)
-right_p = pin3 # Steer Smaller (Green/Right)
-left_p = pin4 # Steer Larger (White/Left)
+right_p = pin3 # Steering Right [Smaller Steering Value] (Green)
+left_p = pin4 # Steering Left [Larger Steering Value] (White)
 
 # ========== Steering Configuation
+# Configurations to limit the steering due to physical steering column limits
 steering_max = 850
 steering_min = 200
-steering_pos = (steering_max - steering_min)/2 + steering_min
+steering_pos = (steering_max - steering_min)/2 + steering_min # Used for simulation and setting default steering position
 
 # ========== Lambda Declaration
+# Lambda declaration for converting a value from one range to another. Used for the gain values
 mapping = lambda value, InitMin, InitMax, NewMin, NewMax : (((NewMax - NewMin)/(InitMax - InitMin)) * (value - InitMax)) + NewMax
 
 # ========== Simulation Code
-similation_status = False
+similation_status = False # Variable to determine whether or not to run simulation
 
+# ========== Function Declaration
 def steering_sim(left, right, lcl_steering_pos):
+    # Local parameters determing simulation speed and adjustment speed of steering
     simulationSpeed = 200
     maxSteeringAdj = 100
     minSteeringAdj = -100
     
+    # Code to check if dual input has been detected. 
+    # If so code is halted and error message is displayed
+    # Else next iteration of simulation is ran
     if bool(left) & bool(right):
         print("ERROR DUAL INPUT Left: {}, Right: {}".format(left, right))
         input()
     else:
-        dir = left-right
-        norm_dir = round(mapping(dir, -1023, 1023, minSteeringAdj, maxSteeringAdj), 2)
+        dir = left-right # Determine the requested steering value
+        norm_dir = round(mapping(dir, -1023, 1023, minSteeringAdj, maxSteeringAdj), 2) 
+        # Convert steering request to adjustment value
+        # Then make sure min/max adjustment value is not exceeded
         if norm_dir > maxSteeringAdj:
             norm_dir = maxSteeringAdj
         elif norm_dir < minSteeringAdj:
             norm_dir = minSteeringAdj
-            
+        
+        # Adjust the simulated steering value according to the adjustment
         lcl_steering_pos += norm_dir
-
+        
+        # Check to make sure the steering min/max is not exceeded
         if lcl_steering_pos > 1023:
             print("Steering Max Exceeded")
         elif lcl_steering_pos < 0:
             print("Steering Min Exceeded")
         
+        # Precents the simulation from running too quickly
         sleep(simulationSpeed)
         return lcl_steering_pos
 
-
 # ========== PID Code
+# Initally set values to 0 if no value is obtained from controller
 gbl_eP = 0
 gbl_eI = 0
 gbl_eD = 0
 prev_timestamp = 0
 
 def PID_algorithm(target, position, integral_error, delta_time, prev_error, Kp, Ki, Kd):
-    error = target - position
-    integral_error += (error * delta_time)
-    derivative_error = (error - prev_error)/ delta_time
+    # Determing the adjustment to be made to steering via a PID algorithm
+    error = target - position # Proportional: Get the error value between the target/requested and the actual position
+    integral_error += (error * delta_time) # Integral: Times the error by the time between iterations. Add the value to global variable.
+    derivative_error = (error - prev_error)/ delta_time # Derivative: Calculate the rate the algorithm is adjusting
 
-    output = (Kp * error) + (Ki * integral_error) + (Kd * derivative_error)
+    output = (Kp * error) + (Ki * integral_error) + (Kd * derivative_error) # Sum the error values timesed by their gain values
     return output, error, integral_error, derivative_error
 
 # ========== Function Declaration
 def limit_func(target, min, max):
+    # Function to limit a value to a range
     if target > max:
         return max
     elif target < min:
@@ -81,7 +95,8 @@ def limit_func(target, min, max):
 # ========== Main Code 
 while True:
     # ========== Variable Reset
-    controls = ""
+    # Clear the requested, gain and pin values.
+    controls = "" 
     requ_steer = 0
     norm_requ_steer = 0
     
@@ -96,6 +111,7 @@ while True:
 
     # ========== Radio Message Detection
     while True:
+        # Keep looping until a Radio message is received
         controls = radio.receive()
         if controls:
             break
@@ -103,6 +119,7 @@ while True:
 
     # ========== Message Assignment
     for i, cont in enumerate(controls):
+        # Assign requested values from Radio message to apppropriate local values
         if 'Gd' in cont:
             temp = cont.split(':')
             Gd = float(temp[1])
@@ -115,6 +132,7 @@ while True:
             temp = cont.split(':')
             requ_steer = mapping(int(temp[1]), -1023, 1023, steering_min, steering_max)
             norm_requ_steer = limit_func(requ_steer, steering_min, steering_max)
+            # Converts requested steering to local steering limits to prevent requests above steering min/max
 
         if 'Gi' in cont:
             temp = cont.split(':')
@@ -133,27 +151,30 @@ while True:
     curr_timestamp = running_time()
     deltaT = curr_timestamp - prev_timestamp
     prev_timestamp = curr_timestamp
+    # Gets the delta time for the PID algorithm and sets variables for next iteration
 
     adjustment, gbl_eP, gbl_eI, gbl_eD = PID_algorithm(norm_requ_steer, steering_pos, gbl_eI, deltaT, gbl_eP, Gp, Gi, Gd)
 
     # ========== Steering Adjustments
+    # Sets the left or right value according to the adjustment value returned from the PID algorithm
+    # Unless the steering min/max is excessed which results in the opposing value to be assigned
     if adjustment > 0:
         if steering_pos > steering_max:
             right_value = adjustment
-            inverted = True
         else:
             left_value = adjustment  
     elif adjustment < 0:
         if steering_pos < steering_min:
             left_value = adjustment
-            inverted = True
         else:
             right_value = adjustment
 
+    # Limits the value to ensure pins can be assigned the value
     left_value = limit_func(int(abs(left_value)), 0, 1023)
     right_value = limit_func(int(abs(right_value)), 0, 1023)
     
     # ========== Pin Assignment
+    # Sets the pins accordingly
     forw_p.write_digital(forw_value)
     back_p.write_digital(back_value)
     left_p.write_analog(left_value)
@@ -161,12 +182,15 @@ while True:
 
     # ========== Simulation
     if button_a.was_pressed():
+        # Inverts simulation status if button A is pressed
         similation_status = not(similation_status)
 
     if similation_status == True:
+        # Run simulation and correct steering_pos according to simulation
         steering_pos = steering_sim(left_value, right_value, steering_pos)
     else:
+        # Else read the steering position
         steering_pos = steering_pin.read_analog()
 
-    # ========== Code Testing
-    print("Req: {}, Pos: {}, Adj: {}, P: {}, I: {}, D: {}, Left: {}, Right: {}, Forward: {}, Backward: {}".format(norm_requ_steer, steering_pos, adjustment, gbl_eP * Gp, gbl_eI * Gi, gbl_eD * Gd, left_value, right_value, forw_value, back_value))
+    # ========== Code Debugging
+    print("Req: {}, Pos: {}, Adj: {}, P: {}, I: {}, D: {}, Left: {}, Right: {}, Forward: {}, Backward: {}".format(norm_requ_steer, steering_pos, adjustment, gbl_eP * Gp, gbl_eI * Gi, gbl_eD * Gd, left_value, right_value, forw_value, back_value)) # Prints values for debugging purposes
--- control-algorithm/controller.py
+++ control-algorithm/controller.py
@@ -8,12 +8,16 @@ import radio
 
 # ========== Microbit Config
 radio.config(group=25, power=7, length=128, data_rate=radio.RATE_1MBIT)
+# Configuring Microbit radio to allow for message to be sent
 
+# ========== Function Declaration
 mapping = lambda value, InitMin, InitMax, NewMin, NewMax : (((NewMax - NewMin)/(InitMax - InitMin)) * (value - InitMax)) + NewMax
+# Lambda declaration for converting a value from one range to another. Used for the gain values
 
 # ========== Main Code
 while True:
     # ========== Dictionary Declaration
+    # Dictionary containing requested values from the controller
     controls = {
         'f': 0, 
         'b': 0, 
@@ -24,17 +28,20 @@ while True:
     }
 
     # ========== Gain Adjustments
+    # Adjusting the Gain values to better suit their effect on the PID algorithm
     controls['Gp'] = round(mapping(controls['Gp'], 0, 1023, 1, 10), 2)
     controls['Gi'] = 10 ** (-1 * round(mapping(controls['Gi'], 0, 1023, 6, 0), 2))
     controls['Gd'] = 10 ** (round(mapping(controls['Gd'], 0, 1023, 0, 5), 2))
 
     # ========== Steering Limits
+    # Limits the steering to +/- 1023 to prevent PID algorithm exceeding values
     if controls['s'] < -1023:
         controls['s'] = -1023
     elif controls['s'] > 1023:
         controls['s'] = 1023
 
     # ========== Direction Adjustment
+    # Adjusts forward/backward requests according to buttons pressed
     if button_b.is_pressed():
         controls['f'] = 1
     elif button_a.is_pressed():
@@ -42,5 +49,7 @@ while True:
 
     radio.send(str(controls))
     sleep(20)
+    # Sends the requested values and delays from program to restrict message requests
 
     print("Forward: {}, Backward: {}, Steering: {}, Gp: {}, Gi: {}, Gd: {}".format(controls['f'], controls['b'], controls['s'], controls['Gp'], controls['Gi'], controls['Gd']))
+    # Prints for debugging
